71dc8458e644cf4216b01f9bf95f459e23b6f875
Nacho Vacas <nvacasm@gmail.com>
2018-06-08 10:42:41 +0200

    Agregar archivos de proyecto.

---
+++ TRIP-ON.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TRIP_ON.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TRIP-ON/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TRIP_ON.Global" Language="C#" %>
